004b15b8c85765f5a38f5b7f77198000f02dab05
Anirudh Kulkarni <anirudhkulkarni1997@gmail.com>
2020-05-25 19:06:54 +0530

    urls updated

--- invoices/urls.py
+++ invoices/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import InvoiceViewSet
 
 router = routers.DefaultRouter()
-router.register('invoices', InvoiceViewSet, 'invoices')
+router.register('', InvoiceViewSet, 'invoices')
 
 urlpatterns = router.urls
